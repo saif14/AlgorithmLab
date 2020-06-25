85001eb347e939448c1b95d2fc0fa612bb060ef1
Faruq Hossain <55442194+Faruq-hossain@users.noreply.github.com>
2020-06-25 13:30:07 +0600

    my try
    cheeck..

--- binary search.cpp
+++ binary search.cpp
@@ -1,66 +1,75 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-int MyFun()
+
+void merge(int arr[], int l, int m, int r, int size)
 {
-    
+    int i=l;
+    int j=m+1;
+    int k=l;
+
+    int temp[size];
+
+    while (i<=m && j<=r) {
+        if (arr[i]<= arr[j]) {
+            temp[k]=arr[i];
+            i++;
+            k++;
+        }
+        else {
+            temp[k]=arr[j];
+            j++;
+            k++;
+        }
+    }
+    while (i<= m) {
+        temp[k]=arr[i];
+        i++;
+        k++;
+    }
+
+    while (j<=r) {
+        temp[k]=arr[j];
+        j++;
+        k++;
+    }
+
+    for (int p=l;p<=r;p++) {
+        arr[p]=temp[p];
+    }
 }
-int recursive_binary_search(int arr[],int L, int R, int val)
+//merge sort....................
+void mergeSort(int arr[], int l, int r, int size)
 {
-    int mid = (L+R)/2;
-    if(R>=L)
-    {
-        if(arr[mid]==val)
-        {
-            return mid;
-        }
-        else if(val> arr[mid])
-        {
-            L = mid+1;
-            recursive_binary_search(arr,L,R,val);
-        }
-        else if(val<arr[mid])
-        {
-            R = mid-1;
-            recursive_binary_search(arr,L,R,val);
-        }
+    if (l < r) {
+        int m = (l+r)/2;
+        mergeSort(arr,l,m,size);
+        mergeSort(arr,m + 1,r,size);
+        merge(arr,l,m,r,size);
     }
-    else
-    {
-        return -1;
-    }
-
-}
-
-int iterative_binary_search(int arr[],int L, int R, int val)
-{
-    while(R>=L)
-    {
-        int mid = (L+R)/2;
-        if(val==arr[mid])
-        {
-            return mid;
-        }
-        else if(val<arr[mid])
-        {
-            R = mid-1;
-        }
-        else if(val> arr[mid])
-        {
-            L = mid+1;
-        }
-    }
-    return -1;
-
 }
 
 int main()
 {
-    int arr[] = {22,25,28,32,37,43,55,61,76,83,87,92,102,104};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int index = recursive_binary_search(arr,0,size-1,104);
-    (index ==-1)? cout<<"element not found" : cout<<"element found. index is: "<<index<<endl;
-    index = iterative_binary_search(arr,0,size-1,92);
-    (index ==-1)? cout<<"element not found" : cout<<"element found. index is: "<<index<<endl;
+    cout << "Enter size of array: " << endl;
+    int size;
+    cin >> size;
+    int narr[size];
 
+    cout << "Enter " << size << " integers in any order: " << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> narr[i];
+    }
+    cout << "Before Sorting" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << narr[i] << " ";
+    }
+    cout << endl;
+    mergeSort(narr, 0, (size - 1), size);
+
+    cout << "After Sorting" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << narr[i] << " ";
+    }
+
+    return 0;
 }
