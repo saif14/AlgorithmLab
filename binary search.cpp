8d7db18208093d4e0002d63c3d869da74ceb132d
Rko Shahed Hossain Aahil <52546342+Rkoshahed@users.noreply.github.com>
2020-06-27 04:22:02 +0600

    Update binary search.cpp

--- binary search.cpp
+++ binary search.cpp
@@ -1,68 +1,42 @@
-shahed hossain
-191-15-2659
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-int MyFun()
-{
-    
-}
-int recursive_binary_search(int arr[],int L, int R, int val)
-{
-    int mid = (L+R)/2;
-    if(R>=L)
-    {
-        if(arr[mid]==val)
-        {
-            return mid;
-        }
-        else if(val> arr[mid])
-        {
-            L = mid+1;
-            recursive_binary_search(arr,L,R,val);
-        }
-        else if(val<arr[mid])
-        {
-            R = mid-1;
-            recursive_binary_search(arr,L,R,val);
-        }
-    }
-    else
-    {
-        return -1;
-    }
-
-}
-
-int iterative_binary_search(int arr[],int L, int R, int val)
-{
-    while(R>=L)
-    {
-        int mid = (L+R)/2;
-        if(val==arr[mid])
-        {
-            return mid;
-        }
-        else if(val<arr[mid])
-        {
-            R = mid-1;
-        }
-        else if(val> arr[mid])
-        {
-            L = mid+1;
-        }
-    }
-    return -1;
-
-}
 
 int main()
 {
-    int arr[] = {22,25,28,32,37,43,55,61,76,83,87,92,102,104};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int index = recursive_binary_search(arr,0,size-1,104);
-    (index ==-1)? cout<<"element not found" : cout<<"element found. index is: "<<index<<endl;
-    index = iterative_binary_search(arr,0,size-1,92);
-    (index ==-1)? cout<<"element not found" : cout<<"element found. index is: "<<index<<endl;
+	int count, i, arr[30], num, first, last, middle;
+	cout<<"how many elements would you like to enter?:"; 
+        cin>>count;
 
+	for (i=0; i<count; i++)
+	{
+		cout<<"Enter number "<<(i+1)<<": "; 
+                cin>>arr[i];
+	}
+	cout<<"Enter the number that you want to search:"; 
+        cin>>num;
+	first = 0;
+	last = count-1;
+	middle = (first+last)/2;
+	while (first <= last)
+	{
+	   if(arr[middle] < num)
+	   {
+		first = middle + 1;
+
+	   }
+	   else if(arr[middle] == num)
+	   {
+		cout<<num<<" found in the array at the location "<<middle+1<<"\n"; 
+                break; 
+           } 
+           else { 
+                last = middle - 1; 
+           } 
+           middle = (first + last)/2; 
+        } 
+        if(first > last)
+	{
+	   cout<<num<<" not found in the array";
+	}
+	return 0;
 }
