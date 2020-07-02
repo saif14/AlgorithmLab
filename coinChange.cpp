337c25a1010ad102675cdc56bc0229e08158dbb7
mokhlesurraham <67578933+mokhlesurraham@users.noreply.github.com>
2020-07-02 00:22:20 -0700

    Update coinChange.cpp

--- coinChange.cpp
+++ coinChange.cpp
@@ -1,10 +1,8 @@
+#include<bits/stdc++.h>
 #include <iostream>
-
 using namespace std;
-
 int coins[] = { 1,5,10,25,100 };
 
-//This function finds the maximum number of coin by selecting the best choice at hand
 int findMaxCoin(int amount, int size){
     for(int i=0; i<size; i++){
         if(amount < coins[i]) return i-1;
@@ -12,31 +10,27 @@ int findMaxCoin(int amount, int size){
     return -1;
 }
 
-//This the greedy approach implementation.
-//This finds the selected coins to fulfill an amount
 int GreedyApproachCoinChagne(int amount, int change[]){
 
     int numOfCoins = sizeof(coins)/sizeof(coins[0]);
     int count = 0;
-    while(//impelemnt the loop condition){
+    while(amount){
         int k = findMaxCoin(amount, numOfCoins);
         if(k == -1)
-                //implement your code
-                //implement what should it print if solution found
+                cout<< "No viable solution";
+                
         else{
-                //Implement your code to minus a coin from the total amount. If total amount is 60 and a coin chosen is 15 then it will be 60-15 = 45
-                // Implement your code to save this coin in the array name 'change'
+                amount=amount- coins[k];
+        change[count++] = coins[k];
+              
             }
     }
     return count;
 }
-        
-
 int main(void) {
-    int change[10]; // This needs to be dynamic
+    int change[10]; 
     int amount = 34;
     int count = GreedyApproachCoinChagne(amount, change);
-
     cout<< "\n Number of coins for change of " << amount<< " : " << count;
     cout<< "\n Coins : ";
     for(int i=0; i<count; i++)
@@ -44,3 +38,4 @@ int main(void) {
         cout<< change[i]<< " ";
     }
 }
+
