546139692fe630d40c740839448775628caa2b51
Rashiduzzaman <67304288+Rashiduzzaman2655@users.noreply.github.com>
2020-07-23 15:12:37 +0600

    Code implementation

--- 0_1_Knapsack.cpp
+++ 0_1_Knapsack.cpp
@@ -1,15 +1,23 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
+int knapSack(int v[], int w[], int n, int W);
 int main()
 {
-   int itemWeight[4] = {2,3,4,5};
-   int itemValue[4] = {3,4,5,6};
-   int bagMaxWeight = 6;
+    int itemWeight[4] = {2,3,4,5};
+    int itemValue[4] = {3,4,5,6};
+    int bagMaxWeight = 6;
+    int n = sizeof(itemWeight) / sizeof(itemWeight[0]);
+    cout << "Knapsack value is " << knapSack(itemWeight, itemValue, n - 1, bagMaxWeight);
+    return 0;
+}
 
-   //implement your code
-   //you may use your own defined function
-   //print out the table filled up with values as I have demonstrated in the class
-
+int knapSack(int v[], int w[], int n, int W)
+{
+    if (W < 0)
+        return INT_MIN;
+    if (n < 0 || W == 0)
+        return 0;
+    int in = v[n] + knapSack(v, w, n - 1, W - w[n]);
+    int ex = knapSack(v, w, n - 1, W);
+    return max (in, ex);
 }
